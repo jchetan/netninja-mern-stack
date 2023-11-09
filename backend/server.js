b2fb69567421cc2c27644b6d2c50f1a5b18f2652
Chetan J <janachet@amazon.com>
2023-11-09 19:01:00 +0530

    restored netninja code

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
+const workoutRoutes = require('./routes/routes')
 
 const app = express();
 
-app.listen(5001, () => {
-    console.log('listening on port 5001')
-});
+app.use(express.json());
+app.use(
+    (req, res, next) => {
+        console.log(req.path, req.method);
+        next();
+    }
+);
 
-app.get('/', (req, res) => res.json('API Running') );
+app.use('/api', workoutRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on port ', process.env.PORT)
+});
