47f50fc2e7fc54de34d305d53635c1acc278353a
Chetan J <janachet@amazon.com>
2023-11-09 18:40:23 +0530

    made code simpler

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,9 @@
-require('dotenv').config();
-
 const express = require('express');
-const workoutRoutes = require('./routes/routes')
 
 const app = express();
 
-app.use(express.json());
-app.use(
-    (req, res, next) => {
-        console.log(req.path, req.method);
-        next();
-    }
-);
-
-app.use('/api/workouts', workoutRoutes);
-
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
+app.listen(5001, () => {
+    console.log('listening on port 5001')
 });
+
+app.get('/', (req, res) => res.json('API Running') );
