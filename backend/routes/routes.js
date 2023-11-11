f47979b11bf7a9d36165090e771c1b9c9f046e31
Chetan J <janachet@amazon.com>
2023-11-11 14:24:58 +0530

    backend completed, frontend skeleton app created

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,18 +1,96 @@
 const express = require('express');
-
+const Workout = require('../models/Workout');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 router.get(
     '/',
-    (req, res) => {
-        res.json({msg: 'Get all workouts'});
+    async (req, res) => {
+        try {
+            const workouts = await Workout.find({});
+            res.status(200).json(workouts);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
 );
 
 router.get(
     '/:id',
-    (req, res) => {
-        res.json({msg: 'Get one workout'});
+    async (req, res) => {
+        const {id} = req.params;
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            try {
+                const workout = await Workout.findById(id);
+                if (!workout) {
+                    res.status(404).json({ error: 'No such workout' });
+                } else {
+                    res.status(200).json(workout);
+                }            
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        } else {
+            res.status(404).json({ error: 'No such workout' });
+        }
+        
+    }
+);
+
+router.delete(
+    '/:id',
+    async (req, res) => {
+        const {id} = req.params;
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            try {
+                const workout = await Workout.findOneAndDelete({_id: id});
+                if (!workout) {
+                    res.status(404).json({ error: 'No such workout' });
+                } else {
+                    res.status(200).json(workout);
+                }            
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        } else {
+            res.status(404).json({ error: 'No such workout' });
+        }
+        
+    }
+);
+
+router.patch(
+    '/:id',
+    async (req, res) => {
+        const {id} = req.params;
+        const { title, load, reps } = req.body;
+        if (mongoose.Types.ObjectId.isValid(id)) {
+            try {
+                const workout = await Workout.findOneAndUpdate({_id: id}, {title, load, reps});
+                if (!workout) {
+                    res.status(404).json({ error: 'No such workout' });
+                } else {
+                    res.status(200).json(workout);
+                }            
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        } else {
+            res.status(404).json({ error: 'No such workout' });
+        }        
+    }
+);
+
+router.post(
+    '/',
+    async (req, res) => {
+        const { title, load, reps } = req.body;
+        try {
+            const workout = await Workout.create({ title, load, reps });
+            res.status(200).json(workout);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
 );
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/routes')
 
 const app = express();
@@ -13,8 +14,16 @@ app.use(
     }
 );
 
-app.use('/api', workoutRoutes);
+app.use('/api/workouts', workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
-});
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db and listening on port ', process.env.PORT)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+
